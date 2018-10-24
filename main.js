d79fd2f176242a6aeece287f2cc5e868e5d146f5
lumix103 <15038234+lumix103@users.noreply.github.com>
2018-10-24 11:29:30 -0700

    On Part4
    I realized in part 3 when I upload it I forgot to make some changes, but oh well they're fixed here.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-//Following Lesson 3: Conditional Rendering
-//Video Challenge: Adda an onSale property to your data object, then use v-if to display that says "On sale!"
-//whenever onSale is true.
+//Following Lesson 4: List Rendering
+//Video Challenge: Add an array of sizes to your data object, then use v-for to display them as a list.
 var app = new Vue(
     {
         el: '#app',
@@ -9,6 +8,20 @@ var app = new Vue(
             product: `Socks`,
             image: './assets/vmSocks-green-onWhite.jpg',
             inventory: 4,
-            onSale: true
+            onStock: true,
+            details:["80% cotton,","20% polyester","Gender-neutral"],
+            variants:
+            [
+                {
+                 variantsId:2234,
+                 variantColor: "green"
+                }
+                ,
+                {
+                 variantsId:2235,
+                 variantColor: "blue"
+                }
+            ],
+            sizes:[`extra-small`,`small`,`medium`,`large`,`extra-large`]
         }
     })
